7ee228e17a51fab44cda712d4122d0fa4c706b68
Gonzalo Quiroz <147004389+GonzaQuiroz@users.noreply.github.com>
2023-10-14 12:34:16 -0300

    ahora si que si

--- celcius.js
+++ celcius.js
@@ -1,33 +1,27 @@
-function celsiusAFahren(celsius) {
-    return (celsius * 9/5) + 32;
-  };
-   
-  function celsiusAKelvin(celsius) {
-    return celsius + 273.15;
-  };
+function celsiusAF(celsius) {
+  return (celsius * 9/5) + 32;
+}
 
-  function num(valor) {
-    return !isNaN(parseFloat(valor))
-  };
- 
-  function convertirTemp(){
-    let input = prompt("ingrese la temperatura en grados celsius que desea convertir: ")
-  };
+function celsiusAK(celsius) {
+  return celsius + 273.15;
+}
 
+function convertirTemp() {
+  let valor = prompt("Ingrese el valor en grados Celsius que desea convertir:");
 
-  if (num(input)) {
-    const celsius = parseFloat(input);
-    const fahrenheit = celsiusAFarehn(celsius);
-    const kelvin = celsiusAKelvin(celsius);
+  if (!isNaN(valor)) {
+    const celsius = parseFloat(valor);
+    const fahrenheit = celsiusAF(celsius);
+    const kelvin = celsiusAK(celsius);
 
     console.log(`${celsius} grados Celsius son equivalentes a ${fahrenheit} grados Fahrenheit.`);
     console.log(`${celsius} grados Celsius son equivalentes a ${kelvin} grados Kelvin.`);
   } else {
-    console.log("peeeeeenca ese no es un numero .");
+    console.log("Ingresa un numero valido .");
     convertirTemp(); 
-  };
-  
-convertirTemperatura();
+  }
+}
 
+convertirTemp();
   
   
